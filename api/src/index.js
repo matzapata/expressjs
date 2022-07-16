bfad2d380e1c4f37f2238add50250319c4ba80b7
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2022-07-15 21:00:18 -0300

    TS -> JS

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,9 @@
-import bodyParser from "body-parser";
-import express from "express";
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.json({ Hello: "World" });
